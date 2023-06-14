a11b8806353fe7df7ff8e5aeca7526c0ab34a10b
PhanMinhPhuong <132879986+PhanMinhPhuong@users.noreply.github.com>
2023-06-14 15:14:52 +0700

    Update bai1.php

--- bai1.php
+++ bai1.php
@@ -4,57 +4,56 @@ $username = "root";
 $password = "";
 $database = "bai_tap_so_6";
 //connect
-$conn = mysqli_connect($severname,$username,$password);
+$conn = mysqli_connect($severname,$username,$password,$database);
 if(!$conn){
-    die ("kết nối thất bại".mysql_error());}
+    die ("kết nối không thành công".mysql_error());}
 else{
     echo("kết nối thành công");
 }
 
 if(!$database){
-    die ("không tìm thấy CSDL".mysql_error());
+    die ("không tìm thấy dữ liệu".mysql_error());
 }
 else{
-    echo("Có CSDL nha");
+    echo("Đã tìm thấy dữ liệu");
 }
 //create
-$sql_stmt ="CREATE TABLE IF NOT EXISTS sinhvien(
-    MASV char(6) NOT NULL PRIMARY KEY,
-    HOTEN nvarchar(50) NOT NULL,
-    NGAYSINH datetime NOT NULL,
-    LOPHOC char(6) NOT NULL,
-    DIEMTB float NOT NULL
+$sql_stmt ="CREATE TABLE IF NOT EXISTS SinhVien(
+    MaSV char(6) NOT NULL PRIMARY KEY,
+    Hoten nvarchar(50) NOT NULL,
+    Ngaysinh datetime NOT NULL,
+    Lophoc char(6) NOT NULL,
+    DiemTB float NOT NULL
     );";
-$result = mysqli_query($conn,$sql_stmt);
+$result = mysqli_query($conn,$sql_qlsv);
 if(!$result){
-    die ("tạo bảng thất bại".mysql_error());
+    die ("Không tạo được bảng".mysql_error());
 }
 else{
-    echo("Success");
+    echo("Tạo bảng thành công");
 }
 //insert
-$sql_stmt ="INSERT INTO sinhvien(
-    MASV, HOTEN, NGAYSINH, LOPHOC, DIEMTB
+$sql_qlsv ="INSERT INTO SinhVien(
+    MaSV, Hoten, Ngaysinh, Lophoc, DiemTB
 )
     VALUES
-    ('SV0001','Nguyen Thi A','12/23/2002','K56SD1',8.8 ),
-    ('SV0002','Nguyen Thi B','12/20/2002','K56SD1',10.0 ),
-    ('SV0003','Nguyen Thi C','12/21/2002','K56SD1',8.9 ),
-    ('SV0004','Nguyen Thi D','12/22/2002','K56SD1',3.6 ),
-    ('SV0005','Nguyen Thi E','12/25/2002','K56SD1',7.8 );
+    ('SV0001','Phan Minh Phuong','11/23/2002','K56SD3',8.0 ),
+    ('SV0002','Tran Thi Lan','12/13/2002','K56SD3',9.0 ),
+    ('SV0003','Nguyen Van Hung','06/21/2002','K56SD3',8.9 ),
+    ('SV0004','Hoang Quoc Huy','02/22/2002','K56SD3',7.5 ),
+    ('SV0005','Doan Xuan Phuc','04/25/2002','K56SD3',7.8 );
     ";
 //update
-$sql_stmt = "UPDATE sinhvien SET DIEMTB = 8.5
-WHERE MASV='SV0001'";
-$result = mysqli_query($conn,$sql_stmt);
+$sql_qlsv = "UPDATE SinhVien SET DiemTB = 8.9 WHERE MaSV ='SV0003'";
+$result = mysqli_query($conn,$sql_qlsv);
 
 //delete
-$sql_stmt = "DELETE FROM sinhvien WHERE MASV='SV0003'";
-$result=mysqli_query($conn,$sql_stmt);
+$sql_qlsv = "DELETE FROM SinhVien WHERE MaSV ='SV0003'";
+$result=mysqli_query($conn,$sql_qlsv);
 if(!$result){
-    die ("xóa thất bại".mysql_error());;
+    die ("Không thể xóa dữ liệu".mysql_error());;
 }
 else{
-    echo("Success");
+    echo("Xóa dữ liệu thành công");
 }
 ?>
